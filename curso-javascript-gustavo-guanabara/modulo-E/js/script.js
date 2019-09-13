5715f4ab52d3994b6032b6f07c85e754a50627fc
Wellingtongu <wellingtonmvzl@yahoo.com.br>
2019-09-12 23:09:12 -0300

    change

---
+++ curso-javascript-gustavo-guanabara/modulo-E/js/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+    let num1 = 1;
+    let num2 = 1;
+
+    while (num1 <= 20) {
+        console.log(`valor de num é ${num1}`);
+        num1 += 1;
+
+    }
+
+    do {
+        console.log(`do while =>  ${num2}`);
+        num2 += 1;
+    } while (num2 <= 10)
+
+}
---
+++ curso-javascript-gustavo-guanabara/modulo-E/js/script2.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+
+    console.log('inicio');
+
+    for (var i = 0; i <= 10; i++) {
+        console.log(`o valor de i é ${i}`);
+
+    }
+    console.log('fim');
+    // let usuarios = [
+    //     'wellington',
+    //     'joão',
+    //     'janaina'
+    // ]
+
+
+
+    // usuarios.forEach(item => {
+    //     console.log(item);
+    // });
+
+
+    // var obj = { name: "John", age: "wewew", city: "New York" };
+
+    // console.log(obj.name);
+
+
+
+}
